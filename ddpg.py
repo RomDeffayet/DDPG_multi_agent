b8c28bf543639d3168561475db875ba332b0d7dc
Romain Deffayet <romain.deffayet@telecom-paristech.fr>
2018-11-15 11:50:51 +0100

    Code structured and functions to build the networks and generate the noise added

--- ddpg.py
+++ ddpg.py
@@ -5,31 +5,53 @@ Created on Tue Nov 13 11:04:15 2018
 @author: Romain Deffayet
 """
 
-#env = make_env('simple')
 
-
-####### Hyperparameters #########
-
-
-
-
-#################################
-
-
-
-
-
-
-class Actor():
+class Actor:
+    def __init__(self, eval_actions, target_actions, state_space_size, n_actions):
+        
     
-
+    def buildNetwork(self, in_size,  N_hidden = 50):
+        model = nn.Sequential(
+                    nn.Linear(in_size, N_hidden),
+                    nn.ReLU(),
+                    nn.Linear(N_hidden, self.n_actions),
+                    nn.Tanh()
+                    nn.Softmax()
+                )
+        return model
+        
+        
+    def ornsteinUhlenbeck(self, T, mu = [np.zeros(env.action_space[i].n) for i in range(env.n)], 
+                                   sigma = 0.3, theta = 0.15, dt = 0.01, x0 = np.zeros_like(mu)):
+        m, n = np.size(mu)
+        random_process = np.zeros(T, n, m)
+        random_process[0] = x0
+        for t in range(1,T):
+            random_process[t] = random_process[t-1] + theta*(mu - random_process[t-1])*dt + \
+            sigma*np.sqrt(dt)*rd.normal(0, 1, (n,m))            
+        return random_process
+        
+        
+    def chooseAction(self,state):
+        
+        
+        
+    def learn(self,transition, alpha = 0.001 , gamma = 0.9, tau = 0.01):
+        
+    
+       
     
     
-    
-    
-    
-    
-class Critic():
+class Critic:
+    def __init__():
+        
+        
+        
+    def buildNetwork():
+        
+        
+        
+    def learn():
 
 
 
--- episode.py
+++ episode.py
@@ -5,4 +5,23 @@ Created on Wed Nov 14 14:28:11 2018
 @author: Romain Deffayet
 """
 
+import torch
+import torch.nn as nn
+
+dtype = torch.float
+device = torch.device("cpu")
+
+
+
+####### Hyperparameters #########
+
+
+
+#################################
+
+
 def episode():
+    
+    
+    
+    env.observation_space[i].shape[0]
